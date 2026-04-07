ccd4cfd955c9dfa515baf11c0b31ff0a5bf6b6a3
temploer <121102723@qq.com>
2026-04-07 09:47:19 +0800

    upgrade optus 4.6 model

--- react/src/constants.ts
+++ react/src/constants.ts
@@ -149,6 +149,7 @@ export const DEFAULT_PROVIDERS_CONFIG: { [key: string]: LLMConfig } = {
   // },
   bedrock: {
     models: {
+      'us.anthropic.claude-opus-4-6-v1:0': { type: 'text' },
       'us.anthropic.claude-sonnet-4-5-20250929-v1:0': { type: 'text' },
     },
     url: '',
